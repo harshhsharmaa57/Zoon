b19a2789bc66dac353cc8cfab7d4117e16345859
harshhsharmaa <harshhsharmaa57@gmail.com>
2026-02-07 02:37:18 +0530

    login register routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { createServer } from "node:http";
 
-
 import { connectToSocket } from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.route.js";
 
 const app = express();
 const server = createServer(app);
@@ -12,8 +12,9 @@ const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
 app.use(cors());
-app.use(express.json({limit: "40kb"}));
-app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes);
 
 app.get("/home", (req, res) => {
   return res.json({ hello: "world" });
