f96651b21005786442a37d1e486590bbe3af5f5d
harshhsharmaa <harshhsharmaa57@gmail.com>
2026-02-16 22:36:54 +0530

    error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/auth" element={<Authentication />} />
 
-            <Route path="/:url" element={<VideoMeetComponent/>}/>
+            <Route path="/" element={<VideoMeetComponent/>}/>
           </Routes>
         </AuthProvider>
       </Router>
